18a31c1d62133e6c48bea05033f32eaa4039a34f
Yevhenns <103356318+Yevhenns@users.noreply.github.com>
2022-08-14 16:44:44 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,9 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+for (let i = 0; i < ingredients.length; i += 1) {
+	let li = document.createElement('li');
+  li.textContent = ingredients[i];
+  li.classList.add("item");
+	document.querySelector("#ingredients").appendChild(li);
+};
