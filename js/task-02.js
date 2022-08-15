d93f71d6787b9e57869972fb89288690f8f0e5ce
Yevhenns <103356318+Yevhenns@users.noreply.github.com>
2022-08-15 20:39:17 +0300

    2, 3

--- js/task-02.js
+++ js/task-02.js
@@ -6,9 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
-for (let i = 0; i < ingredients.length; i += 1) {
-	let li = document.createElement('li');
-  li.textContent = ingredients[i];
-  li.classList.add("item");
-  document.querySelector("#ingredients").appendChild(li);
-};
+// for (let i = 0; i < ingredients.length; i += 1) {
+// 	let li = document.createElement('li');
+//   li.textContent = ingredients[i];
+//   li.classList.add("item");
+//   document.querySelector("#ingredients").appendChild(li);
+// };
+const list = document.querySelector("#ingredients");
+const markup = ingredients
+  .map((ingredient) => `<li class="item">${ingredient}</li>`)
+  .join("");
+  list.innerHTML = markup;
--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,20 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+
+
+
+
+const technologies = ["HTML", "CSS", "JavaScript", "React", "Node"];
+const list = document.querySelector(".list");
+
+const markup = technologies
+  .map((technology) => `<li class="list-item">${technology}</li>`)
+  .join("");
+
+// Check the console, you'll see a single string with HTML tags
+console.log(markup);
+
+// Adding all the markup in one operation
+list.innerHTML = markup;
