eacdf779788562d669e74126a7b68c6be0aa12fa
Yevhenns <103356318+Yevhenns@users.noreply.github.com>
2022-08-20 15:50:16 +0300

    Update task-06.js

---
+++ js/task-06.js
@@ -0,0 +1,46 @@
+// Напиши скрипт, який під час втрати фокусу на інпуті(подія blur), перевіряє його вміст щодо
+// правильної кількості введених символів.
+
+// <input
+//   type="text"
+//   id="validation-input"
+//   data-length="6"
+//   placeholder="Please enter 6 symbols"
+// />
+
+// Яка кількість смиволів повинна бути в інпуті, зазначається в його атрибуті data-length.
+// Якщо введена правильна кількість символів, то border інпуту стає зеленим, якщо неправильна кількість - червоним.
+// Для додавання стилів використовуй CSS-класи valid і invalid, які ми вже додали у вихідні файли завдання.
+
+// #validation-input {
+//   border: 3px solid #bdbdbd;
+// }
+
+// #validation-input.valid {
+//   border-color: #4caf50;
+// }
+
+// #validation-input.invalid {
+//   border-color: #f44336;
+// }
+
+const textInput = document.querySelector("#validation-input");
+console.log(textInput)
+textInput.addEventListener('blur', (event) => {
+  if (textInput.textContent.length = )
+})
+
+// const setFocusBtn = document.querySelector('[data-action="set"]');
+// const removeFocusBtn = document.querySelector('[data-action="remove"]');
+// setFocusBtn.addEventListener("click", () => {
+//   textInput.focus();
+// });
+// removeFocusBtn.addEventListener("click", () => {
+//   textInput.blur();
+// });
+// textInput.addEventListener("focus", () => {
+//   textInput.value = "This input has focus";
+// });
+// textInput.addEventListener("blur", () => {
+//   textInput.value = "";
+// });
